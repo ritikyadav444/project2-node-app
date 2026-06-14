7e96fa6a008a4155f7eca28f4b735df1cdf78879
RITIK YADAV <84266407+ritikyadav444@users.noreply.github.com>
2026-06-14 23:42:24 +0530

    addded

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Project 2 CI/CD Pipeline is Working!');
+    res.send('Hello, World!' + ' This is a simple Express server.' + ' It listens on port 3000 and responds to GET requests at the root URL.');
 });
 
 app.listen(3000, () => {
