98646a70dbfddf585f1a6b4bbf8245627bbf16e7
RITIK YADAV <84266407+ritikyadav444@users.noreply.github.com>
2026-06-14 23:43:48 +0530

    version 5

--- app/server.js
+++ app/server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!' + ' This is a simple Express server.' + ' It listens on port 3000 and responds to GET requests at the root URL.');
+    res.send('Hello, World!' + ' This is a simple Express server.' + ' It listens on port 3000 and responds to GET requests at the root URL.'+ 
+        'You can customize this response to include more information or serve different content as needed.');
 });
 
 app.listen(3000, () => {
